adf3b16fc811ec175be8439979a74d949649d877
Ruchir <ruchirpatel2000@gmail.com>
2023-09-05 21:47:27 +0930

    fixed one bug in q1

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -3,21 +3,29 @@
 
 int main() {
     // Create an Appliance object with a power rating of 100
-    Appliance fridge(100);
+    Appliance* appliance = new Appliance(100);
 
     // Get and set the power rating
-    std::cout << "Fridge Power Rating: " << fridge.getPowerRating() << " watts" << std::endl;
-    fridge.setPowerRating(150);
-    std::cout << "Updated Fridge Power Rating: " << fridge.getPowerRating() << " watts" << std::endl;
+    std::cout << "Initial power rating: " << appliance->getPowerRating() << " watts" << std::endl;
+    appliance->setPowerRating(200);
+    std::cout << "New power rating: " << appliance->getPowerRating() << " watts" << std::endl;
 
-    // Turn on and off the appliance
-    fridge.turnOn();
-    std::cout << "Fridge is on: " << (fridge.isSwitchedOn() ? "Yes" : "No") << std::endl;
-    fridge.turnOff();
-    std::cout << "Fridge is on: " << (fridge.isSwitchedOn() ? "Yes" : "No") << std::endl;
+    // Check if the appliance is on
+    std::cout << "Is appliance on? " << (appliance->isSwitchedOn() ? "Yes" : "No") << std::endl;
+
+    // Turn on the appliance
+    appliance->turnOn();
+    std::cout << "Is appliance on? " << (appliance->isSwitchedOn() ? "Yes" : "No") << std::endl;
+
+    // Turn off the appliance
+    appliance->turnOff();
+    std::cout << "Is appliance on? " << (appliance->isSwitchedOn() ? "Yes" : "No") << std::endl;
 
     // Calculate and display power consumption
-    std::cout << "Fridge Power Consumption: " << fridge.getPowerConsumption() << " kWh" << std::endl;
+    std::cout << "Appliance Power Consumption: " << appliance->getPowerConsumption() << " kWh" << std::endl;
+
+    // Clean up by deleting the dynamically allocated object
+    delete appliance;
 
     return 0;
 }
